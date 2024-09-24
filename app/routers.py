b655b2356459e67ce5723afa38c9b30d2b665b2a
Victor-Sousa-hub <vitorsantos6277@gmail.com>
2024-09-24 14:54:57 -0300

    Exluido a função log

--- app/routers.py
+++ app/routers.py
@@ -9,33 +9,6 @@ from user_agents  import parse
 main = Blueprint('main',__name__)
 
 
-@main.before_request
-def log_trafic():
-    request_time = time.strftime("[%Y-%b-%d %H:%M]")
-    client_ip = request.remote_addr
-    method = request.method
-    path = request.path
-
-    log_entry = f"{request_time} {client_ip} {method} {path}"
-    print("------------------Requisição recebida----------------------------------")
-    print(log_entry)
-    with open('app/log/trafic_log.txt','a') as log_file:
-        log_file.write(log_entry + '\n')
-
-@main.after_request
-def after_request(response):
-    
-    status = response.status
-    log_entry = f"Status da resposta: {status}"
-    print("----------------------Resposta---------------------")
-    print(log_entry)
-    with open('app/log/trafic_log.txt','a') as log_file:
-        log_file.write(log_entry +'\n')
-    
-    return response
-
-
-
 @main.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
